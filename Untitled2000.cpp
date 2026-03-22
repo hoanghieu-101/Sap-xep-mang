91f5c70298b2c51cdaeedcbde0bdcdbb6fd4d852
hoanghieu-101 <hoanghieuqx2004@gmail.com>
2026-03-22 20:55:58 +0700

    update-sapxepmang

--- Untitled2000.cpp
+++ Untitled2000.cpp
@@ -1,33 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    vector<bool> b(n, true);
-    vector<int> s;
-    for (int i = 0; i < n; i++) 
-        cin >> a[i];
-
-    for (int i = 0; i < n; i++) {
-        int max_val = INT_MIN;
-        int max_idx = -1;
-
-        for (int j = 0; j < n; j++) {
-            if (b[j] == true && a[j] > max_val) {
-                max_val = a[j];
-                max_idx = j;
-            }
-        }
-
-        if (max_idx != -1) {
-            s.push_back(max_val);
-            b[max_idx] = false; 
-        }
+int n,x;
+priority_queue<int> pq;
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        pq.push(x);
     }
-    for (int i = 0; i < n; i++) {
-        cout << s[i] << " ";
+    while(!pq.empty())
+    {
+        cout<<pq.top()<<" ";
+        pq.pop();
     }
     return 0;
 }
